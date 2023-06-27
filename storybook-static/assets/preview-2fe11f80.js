74b10402115341c9e829d2c7c5c994bf84a72b9d
Christian Najera <chris.najera.zambrano@gmail.com>
2023-06-27 12:07:14 -0500

    fix: gitignore folder

--- storybook-static/assets/preview-2fe11f80.js
+++
@@ -1,2 +0,0 @@
-const e={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
-//# sourceMappingURL=preview-2fe11f80.js.map
